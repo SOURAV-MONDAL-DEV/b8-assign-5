1029ae8c8dfbce855b41bb1797e90d380d6b841b
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-08-21 20:16:01 +0600

    added: Calculate total price and show them

--- selectNcalculate.js
+++ selectNcalculate.js
@@ -1,21 +1,25 @@
-console.log("js ok")
 
 
 
-function clickToSelect(name, price){
+function clickToSelect(name, price) {
+
+
     const selection = document.getElementById('selection');
-    
+    const TotalPrice = document.getElementById('TotalPrice');
+    const Discount = document.getElementById('Discount');
+    const GrandTotal = document.getElementById('GrandTotal');
 
+    console.log(name, price)
 
-        console.log(name, price)
+    // let name = nameTag.innerText;
+    let createEle = document.createElement('li');
+    createEle.innerText = name;
+    selection.append(createEle);
 
+    TotalPrice.innerText = (parseFloat(TotalPrice.innerText) + parseFloat(price)).toFixed(2);
 
+    // console.log((parseFloat(TotalPrice.innerText) + parseFloat(price)).toFixed(2))
 
-    
-        // let name = nameTag.innerText;
-        let createEle = document.createElement('li');
-        createEle.innerText = name;
-        selection.append(createEle);
-        // btnTag.disabled = true
-    
+    // btnTag.disabled = true
+
 }
