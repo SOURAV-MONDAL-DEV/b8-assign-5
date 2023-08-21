9a847080b74358080861db461be493f4ccf547bf
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-08-21 21:52:28 +0600

    Open Congrats modal and reload the page by click 'go home' button

--- selectNcalculate.js
+++ selectNcalculate.js
@@ -10,6 +10,7 @@ function clickToSelect(name, price) {
     const TotalPrice = document.getElementById('TotalPrice');
     const Discount = document.getElementById('Discount');
     const GrandTotal = document.getElementById('GrandTotal');
+    const PurchaseButton = document.getElementById('PurchaseButton');
 
     console.log(name, price)
 
@@ -20,34 +21,48 @@ function clickToSelect(name, price) {
 
     TotalPrice.innerText = (parseFloat(TotalPrice.innerText) + parseFloat(price)).toFixed(2);
 
-    if(TotalPrice.innerText >= 200){
+    if (TotalPrice.innerText >= 200) {
         // console.log("besi")
         ApplyButton.disabled = false;
     }
 
 
-    if(cuponInput.value == "SALL200"){
+    if (cuponInput.value == "SALL200") {
         console.log("cupon correct")
-        Discount.innerText = ((20/100)*parseFloat(TotalPrice.innerText)).toFixed(2);        
+        Discount.innerText = ((20 / 100) * parseFloat(TotalPrice.innerText)).toFixed(2);
     }
 
 
     // console.log(parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText))
 
-    GrandTotal.innerText = (parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText) ).toFixed(2);
+    GrandTotal.innerText = (parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText)).toFixed(2);
 
+    PurchaseButton.disabled = false;
 
     // btnTag.disabled = true
 
 }
 
-function applyCupon(){
-    if(cuponInput.value == "SALL200"){
+function applyCupon() {
+    if (cuponInput.value == "SALL200") {
         console.log("cupon correct")
-        Discount.innerText = ((20/100)*parseFloat(TotalPrice.innerText)).toFixed(2);
-        GrandTotal.innerText = (parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText) ).toFixed(2);
-        
+        Discount.innerText = ((20 / 100) * parseFloat(TotalPrice.innerText)).toFixed(2);
+        GrandTotal.innerText = (parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText)).toFixed(2);
+
     }
 }
 
+function showCongrats() {
+    const customAlert = document.getElementById("customAlert");
+    customAlert.style.display = "flex ";
+}
+
+
+const reloadButton = document.getElementById("reloadButton");
+
+// Add a click event listener to the button
+reloadButton.addEventListener("click", function () {
+    // Use the location.reload() method to reload the page
+    location.reload();
+});
 
