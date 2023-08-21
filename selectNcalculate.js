86bb6dfd1dbc61f57814b44937e2534a9c292f19
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-08-21 20:58:58 +0600

    added: all calculation work perfectly

--- selectNcalculate.js
+++ selectNcalculate.js
@@ -5,6 +5,8 @@ function clickToSelect(name, price) {
 
 
     const selection = document.getElementById('selection');
+    const cuponInput = document.getElementById('cuponInput');
+    const ApplyButton = document.getElementById('ApplyButton');
     const TotalPrice = document.getElementById('TotalPrice');
     const Discount = document.getElementById('Discount');
     const GrandTotal = document.getElementById('GrandTotal');
@@ -18,8 +20,34 @@ function clickToSelect(name, price) {
 
     TotalPrice.innerText = (parseFloat(TotalPrice.innerText) + parseFloat(price)).toFixed(2);
 
-    // console.log((parseFloat(TotalPrice.innerText) + parseFloat(price)).toFixed(2))
+    if(TotalPrice.innerText >= 200){
+        // console.log("besi")
+        ApplyButton.disabled = false;
+    }
+
+
+    if(cuponInput.value == "SALL200"){
+        console.log("cupon correct")
+        Discount.innerText = ((20/100)*parseFloat(TotalPrice.innerText)).toFixed(2);        
+    }
+
+
+    // console.log(parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText))
+
+    GrandTotal.innerText = (parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText) ).toFixed(2);
+
 
     // btnTag.disabled = true
 
 }
+
+function applyCupon(){
+    if(cuponInput.value == "SALL200"){
+        console.log("cupon correct")
+        Discount.innerText = ((20/100)*parseFloat(TotalPrice.innerText)).toFixed(2);
+        GrandTotal.innerText = (parseFloat(TotalPrice.innerText) - parseFloat(Discount.innerText) ).toFixed(2);
+        
+    }
+}
+
+
